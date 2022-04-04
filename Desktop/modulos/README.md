560a0b11b15da62787347f2ca8c490e1054fbe98
jose <jose.souza4@hotmail.com>
2022-04-04 15:31:38 -0300

    modulos

---
+++ Desktop/modulos/README.md
@@ -0,0 +1,2 @@
+# Módulos
+Este repositório contém a atividade prática do Curso "Módulos", que faz parte do Basecamp de Javascript que estudei pela Digital Innovation One.
---
+++ Desktop/modulos/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs"
+
+console.log(mostraIdade('Alexandre', 42));
+console.log(mostraCidade('Alexandre', 'Rio de Janeiro'));
+console.log(mostraHobby('Alexandre', 'ciclismo')); 
